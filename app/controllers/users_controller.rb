2fa2fba5f05fa94394bfabd2a4e033736d3d6bdc
Ryuichiro Muraki <ryu6apple@gmail.com>
2020-03-24 14:58:07 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,38 @@
 class UsersController < ApplicationController
-  def new
+  def index
+    @users = User.all
   end
+
+  def new
+    # debugger
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+    # debugger
+  end
+
+  def create
+    # debugger
+    @user = User.create(user_params)
+    if @user.save
+      flash[:success] = "account created! welcome to the sample app"
+      redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,3 +6,5 @@ class User < ApplicationRecord
   validates(:password, presence: true, length: { minimum: 8 } )
   has_secure_password
 end
+
+
